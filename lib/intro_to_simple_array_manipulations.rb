d828bb4c58e050c9a2c229c7839203afd9f29023
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-04 19:48:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, "string")
-  array.insert("string")
+def using_insert(array, element)
+  array.insert(element)
 end 
 
 def using_uniq(array)
